d21f1d28e5772e96db8c4650bb9b45bf897ad457
mineboss003-dotcom <mineboss003@gmail.com>
2026-07-14 17:14:43 +0300

    Add files via upload

--- sakura.js
+++ sakura.js
@@ -1,17 +1,18 @@
-const petals=document.getElementById("petals");
+const petals = document.getElementById("petals");
 
-for(let i=0;i<60;i++){
+for (let i = 0; i < 60; i++) {
+    const petal = document.createElement("div");
 
-const petal=document.createElement("div");
+    petal.className = "petal";
 
-petal.className="petal";
+    petal.style.left = Math.random() * 100 + "vw";
+    petal.style.top = -Math.random() * 100 + "px";
 
-petal.style.left=Math.random()*100+"vw";
+    petal.style.width = (10 + Math.random() * 15) + "px";
+    petal.style.height = petal.style.width;
 
-petal.style.animationDuration=5+Math.random()*8+"s";
+    petal.style.animationDuration = (5 + Math.random() * 8) + "s";
+    petal.style.animationDelay = (Math.random() * 5) + "s";
 
-petal.style.animationDelay=Math.random()*5+"s";
-
-petals.appendChild(petal);
-
+    petals.appendChild(petal);
 }
